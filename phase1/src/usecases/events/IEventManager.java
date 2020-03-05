fa9d6e313bd49dbca74c51e8cb6bc843b9fa5a6d
Michael Lai <michael@motivstud.io>
2020-03-05 16:05:43 -0500

    incremenet event manager and repository

--- phase1/src/usecases/events/IEventManager.java
+++ phase1/src/usecases/events/IEventManager.java
@@ -11,26 +11,26 @@ public interface IEventManager {
     boolean createEvent(CalendarEvent event);
 
     CalendarEvent getEventByID(String id);
-    CalendarEvent[] getEventByIDs(String[] ids);
-    CalendarEvent[] getEventByNameAndUserID(String name, String userID);
+    CalendarEvent[] getEventsByIDs(String[] ids);
+    CalendarEvent[] getEventsByNameAndUserID(String name, String userID);
 
-    CalendarEvent[] getEventByDateAndUserID(Date before, Date after, String userID);
+    CalendarEvent[] getEventsByDateAndUserID(Date before, Date after, String userID);
 
-    CalendarEvent[] getEventByDateBeforeAndUserID(Date before, String userID);
+    CalendarEvent[] getEventsByDateBeforeAndUserID(Date before, String userID);
 
-    CalendarEvent[] getEventByDateAfterAndIDs(Date after, String userID);
+    CalendarEvent[] getEventsByDateAfterAndUserID(Date after, String userID);
 
     // series
     CalendarEvent[] getEventsBySeriesIDAndUserID(String seriesID, String userID);
-    CalendarEvent editEventSeriesID(String eventID, String newSeriesID);
+    boolean editEventSeriesID(String eventID, String newSeriesID);
 
 
     // notes
-    CalendarEvent[] fetchEventsByTagIDAndUserID(String tagID, String userID);
-    CalendarEvent[] fetchEventsByMemoIDAndUserID(String memoID, String userID);
+    CalendarEvent[] getEventsByTagIDAndUserID(String tagID, String userID);
+    CalendarEvent[] getEventsByMemoIDAndUserID(String memoID, String userID);
     //edit
 
     // get event by alertID (and userID)
-    CalendarEvent[] fetchEventsByAlertIDAndUserID(String alertID, String userID);
-    CalendarEvent editEventAlertID(String eventID, String newAlertID);
+    CalendarEvent getEventByAlertIDAndUserID(String alertID, String userID);
+//    CalendarEvent editEventAlertID(String eventID, String newAlertID);
 }
