0277759b362cd4c487722c74d90515127027cfa8
Michael Lai <michael@motivstud.io>
2020-03-05 15:49:12 -0500

    push new event manager interface

--- phase1/src/usecases/events/EventManager.java
+++ phase1/src/usecases/events/EventManager.java
@@ -7,89 +7,68 @@ import java.util.Date;
 class EventManager implements IEventManager {
     private IEventRepository repository;
 
-    EventManager(IEventRepository repository) {
-        this.repository = repository;
-    }
-
-    /**
-     * Create and save a CalendarEvent.
-     * @param event the event to save
-     * @return true if event creation was successful, false otherwise
-     */
     @Override
     public boolean createEvent(CalendarEvent event) {
-        this.repository.saveEvent(event);
-        return true;
+        return false;
     }
 
-    /**
-     * Get a CalendarEvent by its ID.
-     *
-     * @param id the ID to filter by.
-     * @return the corresponding CalendarEvent or null if it does not exist
-     */
     @Override
     public CalendarEvent getEventByID(String id) {
         return null;
     }
 
-    /**
-     * Get all CalendarEvents that have an ID in a list of IDs
-     *
-     * @param ids returned CalendarEvents must have an ID in this list
-     * @return list of matching CalendarEvents
-     */
     @Override
     public CalendarEvent[] getEventByIDs(String[] ids) {
         return new CalendarEvent[0];
     }
 
-    /**
-     * Get all CalendarEvents that have an ID in a list of IDs and a matching name
-     *
-     * @param name events must have this name
-     * @param ids  returned CalendarEvents must have an ID in this list
-     * @return list of matching CalendarEvents
-     */
     @Override
-    public CalendarEvent[] getEventByNameAndIDs(String name, String[] ids) {
+    public CalendarEvent[] getEventByNameAndUserID(String name, String userID) {
+        return new CalendarEvent[0];
+    }
+
+    @Override
+    public CalendarEvent[] getEventByDateAndUserID(Date before, Date after, String userID) {
+        return new CalendarEvent[0];
+    }
+
+    @Override
+    public CalendarEvent[] getEventByDateBeforeAndUserID(Date before, String userID) {
+        return new CalendarEvent[0];
+    }
+
+    @Override
+    public CalendarEvent[] getEventByDateAfterAndIDs(Date after, String userID) {
+        return new CalendarEvent[0];
+    }
+
+    @Override
+    public CalendarEvent[] getEventsBySeriesIDAndUserID(String seriesID, String userID) {
+        return new CalendarEvent[0];
+    }
+
+    @Override
+    public CalendarEvent editEventSeriesID(String eventID, String newSeriesID) {
         return null;
     }
 
-    /**
-     * Get all CalendarEvents that have an ID in a list of IDs in the specified appropriate Date range
-     *
-     * @param before returned CalendarEvents must start before this Date
-     * @param after  returned CalendarEvents must end after this Date
-     * @param ids    returned CalendarEvents must have an ID in this list
-     * @return list of matching CalendarEvents
-     */
     @Override
-    public CalendarEvent[] getEventByDateAndIDs(Date before, Date after, String[] ids) {
-        return null;
+    public CalendarEvent[] fetchEventsByTagIDAndUserID(String tagID, String userID) {
+        return new CalendarEvent[0];
     }
 
-    /**
-     * Get all CalendarEvents that have an ID in a list of IDs that end before a Date
-     *
-     * @param before returned CalendarEvents must end before this Date
-     * @param ids    returned CalendarEvents must have an ID in this list
-     * @return list of matching CalendarEvents
-     */
     @Override
-    public CalendarEvent[] getEventByDateBeforeAndIDs(Date before, String[] ids) {
-        return null;
+    public CalendarEvent[] fetchEventsByMemoIDAndUserID(String memoID, String userID) {
+        return new CalendarEvent[0];
     }
 
-    /**
-     * Get all CalendarEvents that have an ID in a list of IDs that start after a Date
-     *
-     * @param after returned CalendarEvents must start after this Date
-     * @param ids   returned CalendarEvents must have an ID in this list
-     * @return list of matching CalendarEvents
-     */
     @Override
-    public CalendarEvent[] getEventByDateAfterAndIDs(Date after, String[] ids) {
+    public CalendarEvent[] fetchEventsByAlertIDAndUserID(String alertID, String userID) {
+        return new CalendarEvent[0];
+    }
+
+    @Override
+    public CalendarEvent editEventAlertID(String eventID, String newAlertID) {
         return null;
     }
 }
--- phase1/src/usecases/events/IEventManager.java
+++ phase1/src/usecases/events/IEventManager.java
@@ -8,57 +8,29 @@ import entities.CalendarEvent;
  * IEventManager provides a public interface for event operations
  */
 public interface IEventManager {
-    /**
-     * Create and save a CalendarEvent.
-     * @param event the event to save
-     * @return true if event creation was successful, false otherwise
-     */
     boolean createEvent(CalendarEvent event);
 
-    /**
-     * Get a CalendarEvent by its ID.
-     * @param id the ID to filter by.
-     * @return the corresponding CalendarEvent or null if it does not exist
-     */
     CalendarEvent getEventByID(String id);
-
-    /**
-     * Get all CalendarEvents that have an ID in a list of IDs
-     * @param ids returned CalendarEvents must have an ID in this list
-     * @return list of matching CalendarEvents
-     */
     CalendarEvent[] getEventByIDs(String[] ids);
+    CalendarEvent[] getEventByNameAndUserID(String name, String userID);
 
-    /**
-     * Get all CalendarEvents that have an ID in a list of IDs and a matching name
-     * @param name events must have this name
-     * @param ids returned CalendarEvents must have an ID in this list
-     * @return list of matching CalendarEvents
-     */
-    CalendarEvent[] getEventByNameAndIDs(String name, String[] ids);
+    CalendarEvent[] getEventByDateAndUserID(Date before, Date after, String userID);
 
-    /**
-     * Get all CalendarEvents that have an ID in a list of IDs in the specified appropriate Date range
-     * @param before returned CalendarEvents must start before this Date
-     * @param after returned CalendarEvents must end after this Date
-     * @param ids returned CalendarEvents must have an ID in this list
-     * @return list of matching CalendarEvents
-     */
-    CalendarEvent[] getEventByDateAndIDs(Date before, Date after, String[] ids);
+    CalendarEvent[] getEventByDateBeforeAndUserID(Date before, String userID);
 
-    /**
-     * Get all CalendarEvents that have an ID in a list of IDs that end before a Date
-     * @param before returned CalendarEvents must end before this Date
-     * @param ids returned CalendarEvents must have an ID in this list
-     * @return list of matching CalendarEvents
-     */
-    CalendarEvent[] getEventByDateBeforeAndIDs(Date before, String[] ids);
+    CalendarEvent[] getEventByDateAfterAndIDs(Date after, String userID);
 
-    /**
-     * Get all CalendarEvents that have an ID in a list of IDs that start after a Date
-     * @param after returned CalendarEvents must start after this Date
-     * @param ids returned CalendarEvents must have an ID in this list
-     * @return list of matching CalendarEvents
-     */
-    CalendarEvent[] getEventByDateAfterAndIDs(Date after, String[] ids);
+    // series
+    CalendarEvent[] getEventsBySeriesIDAndUserID(String seriesID, String userID);
+    CalendarEvent editEventSeriesID(String eventID, String newSeriesID);
+
+
+    // notes
+    CalendarEvent[] fetchEventsByTagIDAndUserID(String tagID, String userID);
+    CalendarEvent[] fetchEventsByMemoIDAndUserID(String memoID, String userID);
+    //edit
+
+    // get event by alertID (and userID)
+    CalendarEvent[] fetchEventsByAlertIDAndUserID(String alertID, String userID);
+    CalendarEvent editEventAlertID(String eventID, String newAlertID);
 }
