7d35e556176ae6422303a94d166e10bf7939d088
selina <selina.huo@utoronto.ca>
2020-03-06 12:28:09 -0500

    clearify the memoid and userid

--- phase1/src/entities/Memo.java
+++ phase1/src/entities/Memo.java
@@ -5,12 +5,30 @@ import java.util.ArrayList;
 public class Memo{
     String note;
     String name;
-    String id;
+    String memoID;
+    String userID;
 
 
-    public Memo(String name, String note, String id) {
+    public Memo(String name, String note, String memoID, String userID) {
         this.name = name;
         this.note = note;
-        this.id = id;
+        this.memoID = memoID;
+        this.userID = userID;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMemoID() {
+        return memoID;
+    }
+
+    public String getUserID() {
+        return userID;
     }
 }
