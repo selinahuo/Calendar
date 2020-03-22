694fcf2c3f5762b82c1ad79ab4d72e7bd3ea102e
Félix C.T <pewfacepew@gmail.com>
2020-03-22 00:27:00 -0400

    Added tag and Memo classes

---
+++ phase2/src/entities/Memo.java
@@ -0,0 +1,43 @@
+package entities;
+
+import java.io.Serializable;
+
+public class Memo implements Serializable {
+    String note;
+    String name;
+    String memoID;
+    String userID;
+
+    public Memo(String name, String note, String memoID, String userID) {
+        this.name = name;
+        this.note = note;
+        this.memoID = memoID;
+        this.userID = userID;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMemoID() {
+        return memoID;
+    }
+
+    public String getUserID() {return userID;}
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+}
---
+++ phase2/src/entities/Tag.java
@@ -0,0 +1,47 @@
+package entities;
+
+import java.io.Serializable;
+
+public class Tag implements Serializable {
+    private String tagID;
+    private String name;
+    private int count;
+    private String userID;
+
+    public Tag(String tagID, String name, int count, String userID) {
+        this.tagID = tagID;
+        this.name = name;
+        this.count = count;
+        this.userID = userID;
+    }
+
+    public String getTagID() {
+        return tagID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int addCount(){
+        this.count++;
+        return this.count;
+    }
+
+    public int removeCount(){
+        this.count--;
+        return this.count;
+    }
+}
