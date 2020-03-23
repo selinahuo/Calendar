8d70653cbb004a3e054a07837a530e0d3b3c90a8
jeanchen <ging.chen@mail.utoronto.ca>
2020-03-23 09:54:25 -0700

    Phase II Alert classes modified.

---
+++ phase2/src/entities/Alert.java
@@ -0,0 +1,90 @@
+package entities;
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * Class representing  a Calendar Alert
+ */
+public class Alert implements Serializable {
+    private String alertID;
+    private String alertName;
+    private Boolean totalAcknowledged;
+    private String userID;
+    private ArrayList<GregorianCalendar> times = new ArrayList<GregorianCalendar>();
+    private List<Boolean> acknowledge = new ArrayList<Boolean>();
+
+
+    public Alert(String alertID, String alertName, String userID, GregorianCalendar startTime) {
+        this.alertID = alertID;
+        this.alertName = alertName;
+        this.totalAcknowledged = false;
+        this.userID = userID;
+        this.times.add(startTime);
+        this.acknowledge.add(false);
+    }
+
+    public Alert(String alertID, String alertName, String userID,  ArrayList<GregorianCalendar> alertTimes) {
+        this.alertID = alertID;
+        this.alertName = alertName;
+        this.totalAcknowledged = false;
+        this.userID = userID;
+        this.times = alertTimes;
+        for(GregorianCalendar time : times) {
+            acknowledge.add(false);
+        }
+    }
+
+    public String getAlertID() {
+        return alertID;
+    }
+    public void setAlertID(String newAlertID) {this.alertID = newAlertID;}
+    public String getAlertName() {
+        return alertName;
+    }
+    public void setAlertName(String newName) {
+        this.alertName = newName;
+    }
+    public Boolean getTotalAcknowledged() {return this.totalAcknowledged;}
+    public void setAcknowledged() {
+        this.totalAcknowledged = !this.totalAcknowledged;
+    }
+    public String getUserID(){ return this.userID;}
+
+    public GregorianCalendar getNextRing(){
+        if (this.totalAcknowledged) {
+            return null;
+        }
+        int i = 0;
+        for (Boolean acknowledged: acknowledge) {
+            if (!acknowledged) {
+                return times.get(i);
+            }
+            i++;
+        }
+        this.totalAcknowledged = true;
+        return null;
+    }
+
+    public List<GregorianCalendar> getTimes(){
+        return this.times;
+    }
+
+    public List<Boolean> getAcknowledge(){
+        return this.acknowledge;
+    }
+    public void setAcknowledge(List<Boolean> acknowledge) {
+        this.acknowledge = acknowledge;
+    }
+
+    public void acknowledge(){
+        if (!this.totalAcknowledged) {
+            int i = 0;
+            while (this.getAcknowledge().get(i)) {
+                i++;
+            }
+            this.getAcknowledge().set(i, true);
+        }
+
+    }
+
+}
---
+++ phase2/src/usecases/alerts/IAlertRepository.java
@@ -0,0 +1,69 @@
+package usecases.alerts;
+import entities.Alert;
+
+import java.util.List;
+
+/**
+ * Provides a public interface for alert repository operations
+ */
+public interface IAlertRepository {
+    /**
+     * Saves an alert.
+     * @param alert the alert to save
+     * @return true if event creation was successful, false otherwise
+     */
+    boolean saveAlert(Alert alert);
+
+    /**
+     *
+     * @param alertID
+     * @param name
+     * @param newStart
+     * @return
+     */
+//    boolean editIndividualAlert(String alertID, String name, GregorianCalendar newStart);
+
+    /**
+     *
+     * @param alertID
+     * @param name
+     * @param frequency
+     * @return
+     */
+//    boolean editFrequencyAlerts(String alertID, String name, ArrayList<GregorianCalendar> frequency);
+
+
+
+    /**
+     * Fetch an Alert by their ID.
+     * @param id the ID to filter by.
+     * @return the corresponding Alert or null if it does not exist
+     */
+//    Alert fetchAlertByID(String id);
+
+    /**
+     * Fetch a Alert by its Name.
+     * @param name the name to filter by.
+     * @param userID involves in this alert
+     * @return the corresponding alert or null if it does not exist
+     */
+//    Alert fetchAlertByNameAndUserID(String name, String userID);
+
+    /**
+     *
+     * @param alertID
+     * @param userID involves in this alert
+     * @return
+     */
+    Alert fetchAlertByIDAndUserID(String alertID, String userID);
+
+//    IndividualAlert fetchIndividualAlertByID(String alertID);
+//    FrequencyAlert fetchFrequencyAlertByID(String alertID);
+
+    Alert[] fetchAlertByUserID(String userID);
+
+    boolean editAlertAcknowledge(String alertID, List<Boolean> acknowledge);
+    boolean deleteAlert(String AlertID, String OwnerID);
+}
+
+
