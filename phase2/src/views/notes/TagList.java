f551b513f98a2496ecbc654f0bb8bff6c6da6fed
Félix Cardinal Tremblay <pewfacepew@gmail.com>
2020-03-31 14:50:20 -0400

    Fixed the Note views, added memo/tag list classes

--- phase2/src/views/notes/TagList.java
+++ phase2/src/views/notes/TagList.java
@@ -5,25 +5,24 @@ import controller.viewmodels.ListModel;
 import views.ListView;
 import views.LocalStorage;
 import views.View;
-import views.events.EventMenu;
 
 import java.util.Scanner;
 
-public class NoteList extends ListView {
+public class TagList extends ListView {
 
-    public NoteList(LocalStorage localStorage, ListModel model, Controller controller) {
+    public TagList(LocalStorage localStorage, ListModel model, Controller controller) {
         super(localStorage, model, controller);
     }
 
     private void inputPrompt() {
         System.out.println("Please select one of the following choices by entering a number:");
-//        System.out.println("[1] View individual event");
-        System.out.println("[~] Back to event menu");
+//        System.out.println("[1] View individual note");
+        System.out.println("[~] Back to Tag menu");
     }
 
     @Override
     public View run() {
-        printTitle("Event List");
+        printTitle("Tag List");
         Scanner input = new Scanner(System.in);
 
         printList();
