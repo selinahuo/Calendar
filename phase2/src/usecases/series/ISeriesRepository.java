96125257380e81dbb7ce66fc0dcd357aa51a7f44
selinaa_128 <selinahuo2018@gmail.com>
2020-04-09 17:57:20 -0400

    * Add documentation

--- phase2/src/usecases/series/ISeriesRepository.java
+++ phase2/src/usecases/series/ISeriesRepository.java
@@ -5,22 +5,88 @@ import entities.Tag;
 
 import java.util.ArrayList;
 
+/**
+ * Provides a public interface for series repository operations
+ */
 public interface ISeriesRepository {
 
     // Save
+
+    /**
+     * Save a Series.
+     *
+     * @param series
+     */
     void saveSeries(Series series);
 
     // Fetch
+
+    /**
+     * Returns a list of Series by userID.
+     *
+     * @param userID
+     * @return
+     */
     ArrayList<Series> fetchSeriesByUserID(String userID);
+
+    /**
+     * Returns a list of Series by Series's name and userID.
+     *
+     * @param seriesName
+     * @param userID
+     * @return
+     */
     ArrayList<Series> fetchSeriesBySeriesNameAndUserID(String seriesName, String userID);
+
+    /**
+     * Returns a single Series by SeriesID and userID.
+     *
+     * @param seriesID
+     * @param userID
+     * @return
+     */
     Series fetchSeriesBySeriesIDAndUserID(String seriesID, String userID);
 
     // Edit
+
+    /**
+     * Returns whether the modification of Series's name is successful.
+     *
+     * @param seriesID
+     * @param seriesName
+     * @param OwnerID
+     * @return
+     */
     boolean editSeriesName(String seriesID, String seriesName, String OwnerID);
+
+    /**
+     * Returns whether adding the Series's event count is successful.
+     *
+     * @param seriesID
+     * @param eventCount
+     * @param ownerID
+     * @return
+     */
     boolean editSeriesEventCount(String seriesID, int eventCount, String ownerID);
+
+    /**
+     * Returns whether subtracting the Series's event count is successful.
+     *
+     * @param seriesID
+     * @param ownerID
+     * @return
+     */
     boolean editSeriesEventCountRemove(String seriesID, String ownerID);
 
     // Delete
+
+    /**
+     * Returns whether Series deletion is successful.
+     *
+     * @param seriesID
+     * @param ownerID
+     * @return
+     */
     boolean deleteSeries(String seriesID, String ownerID);
 
 }
--- phase2/src/usecases/series/SeriesManager.java
+++ phase2/src/usecases/series/SeriesManager.java
@@ -8,16 +8,33 @@ import usecases.events.IEventDeletionObserver;
 import java.util.ArrayList;
 import java.time.LocalDateTime;
 
+/**
+ * A class that is responsible for the creation and modification of series instances in the Calendar program.
+ */
 public class SeriesManager implements IEventDeletionObserver {
     private ISeriesRepository repository;
     private EventManager eventManager;
 
+    /**
+     * Constructor for SeriesManager.
+     *
+     * @param repository
+     * @param eventManager
+     */
     public SeriesManager(ISeriesRepository repository, EventManager eventManager) {
         this.repository = repository;
         this.eventManager = eventManager;
     }
 
-    // save
+    //save
+    /**
+     * Create a Series by combining events.
+     *
+     * @param seriesName
+     * @param eventIDs
+     * @param userID
+     * @return
+     */
     public boolean createSeriesByCombiningEvents(String seriesName, ArrayList<String> eventIDs, String userID){
         Series newSeries = new Series(seriesName, 0, userID);
         repository.saveSeries(newSeries);
@@ -36,6 +53,17 @@ public class SeriesManager implements IEventDeletionObserver {
         return false;
     }
 
+    /**
+     * Create a Series from Event formula.
+     *
+     * @param seriesName
+     * @param start
+     * @param end
+     * @param frequency
+     * @param numEvents
+     * @param userID
+     * @return
+     */
     public boolean createSeriesFromEventFormula(String seriesName, LocalDateTime start, LocalDateTime end, String frequency, int numEvents, String userID){
         Series newSeries = new Series(seriesName, numEvents, userID);
         ArrayList<LocalDateTime[]> times = getTimes(start, end, frequency, numEvents);
@@ -47,6 +75,15 @@ public class SeriesManager implements IEventDeletionObserver {
         return true;
     }
 
+    /**
+     * get times
+     *
+     * @param start
+     * @param end
+     * @param frequency
+     * @param numEvents
+     * @return
+     */
     private ArrayList<LocalDateTime[]> getTimes(LocalDateTime start, LocalDateTime end, String frequency, int numEvents) {
         int daysToAdd;
         if (frequency.equals("d")) {
@@ -71,21 +108,48 @@ public class SeriesManager implements IEventDeletionObserver {
     }
 
     // singular
+    /**
+     * Get a single Series by SeriesID and userID.
+     *
+     * @param seriesID
+     * @param userID
+     * @return
+     */
     public Series getSeriesBySeriesIDAndUserID(String seriesID, String userID){
         return repository.fetchSeriesBySeriesIDAndUserID(seriesID, userID);
     }
 
     // plural
+    /**
+     * Get multiple Series by userID.
+     *
+     * @param userID
+     * @return
+     */
     public ArrayList<Series> getSeriesByUserID(String userID){
         return repository.fetchSeriesByUserID(userID);
     }
 
+    /**
+     * Get Series by Series name.
+     *
+     * @param seriesName
+     * @param userID
+     * @return
+     */
     public ArrayList<Series> getSeriesBySeriesName(String seriesName, String userID){
         return repository.fetchSeriesBySeriesNameAndUserID(seriesName, userID);
     }
 
-
     // edit
+    /**
+     * Edit Series's name.
+     *
+     * @param seriesID
+     * @param seriesName
+     * @param userID
+     * @return
+     */
     public boolean editSeriesName(String seriesID, String seriesName, String userID){
         return repository.editSeriesName(seriesID, seriesName, userID);
     }
