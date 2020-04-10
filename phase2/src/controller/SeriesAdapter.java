0dbe9f02a36521c147a063590ce807e9552ac143
selinaa_128 <selinahuo2018@gmail.com>
2020-04-10 14:09:00 -0400

    modified documentation

--- phase2/src/controller/SeriesAdapter.java
+++ phase2/src/controller/SeriesAdapter.java
@@ -6,12 +6,25 @@ import entities.Series;
 import java.util.ArrayList;
 
 class SeriesAdapter {
-    static String createSeriesString(Series series){
+
+    /**
+     * A private static method that allows translating series status to a string representation.
+     *
+     * @param series the series instance that would be processed
+     * @return a String representation fo the status of a series instance
+     */
+    private static String createSeriesString(Series series){
         return String.format("ID: %s | Name: %s | Event Count: %d, | User: %s",
                 series.getSeriesID(),series.getSeriesName(), series.getEventCount(), series.getUserID());
     }
 
-    static ListModel createSeriesListModel(ArrayList<Series> series){
+    /**
+     * A private method that allows translating series information to a Series representation.
+     *
+     * @param series the series instance that would be precessed
+     * @return A list of model that has the information of the series in the input list
+     */
+    private static ListModel createSeriesListModel(ArrayList<Series> series){
         ArrayList<String> seriesList = new ArrayList<>();
         for (Series individualSeries : series){
             seriesList.add(createSeriesString(individualSeries));
