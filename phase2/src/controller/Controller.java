24f7d5219af6a88e21295b3c856faa07334d5158
Michael Lai <michael@motivstud.io>
2020-04-11 22:31:07 -0600

    document controller class

--- phase2/src/controller/Controller.java
+++ phase2/src/controller/Controller.java
@@ -13,9 +13,18 @@ import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.ArrayList;
 
+/**
+ * Class responsible for "adapting" user input and output and forwarding user requests to use case layer.
+ * Adapts data for presentation and to form most convenient for use cases or user interfaces.
+ */
 public class Controller {
     private UseCaseManager useCaseManager;
 
+    /**
+     * Construct a Controller
+     *
+     * @param useCaseManager injected UseCaseManager instance
+     */
     public Controller(UseCaseManager useCaseManager) {
         this.useCaseManager = useCaseManager;
     }
@@ -89,110 +98,338 @@ public class Controller {
     }
 
     // EVENTS
+
+    /**
+     * Create an event
+     * @param name event name
+     * @param start start date string
+     * @param end end date string
+     * @param location event location
+     * @param userID ID of user creating event
+     * @return eventID if successful, or null otherwise
+     */
     public String createEvent(String name, String start, String end, String location, String userID) {
-        return useCaseManager.createEvent(name, GlobalAdapter.stringToDateTime(start), GlobalAdapter.stringToDateTime(end),
-                location, userID);
+        return useCaseManager.createEvent(name, GlobalAdapter.stringToDateTime(start),
+                GlobalAdapter.stringToDateTime(end), location, userID);
     }
+
+    /**
+     * Create a holiday using the first weekday of a specific month
+     *
+     * @param name event name
+     * @param year year of the event
+     * @param month month of the event
+     * @param weekDay which first weekday of month
+     * @param location event location
+     * @param userID ID of user creating event
+     * @return event ID if successful; null otherwise
+     */
     public String createHolidayEvent(String name, int year, int month, String weekDay, String location, String userID) {
         return useCaseManager.createEventByFirstWeekDay(name, year, GlobalAdapter.intToMonth(month),
                 GlobalAdapter.stringToWeekDay(weekDay), location, userID);
     }
+
+    /**
+     * Get a singular (view) model for an event
+     *
+     * @param eventID ID of event
+     * @param userID ID of associated used
+     * @return singular event model
+     */
     public SingularModel getSingularEvent(String eventID, String userID) {
-        Sextuple<CalendarEvent, Memo, ArrayList<Tag>, Alert, Calendars, Series> meta = useCaseManager.getSingularEvent(eventID, userID);
+        Sextuple<CalendarEvent, Memo, ArrayList<Tag>, Alert, Calendars, Series> meta =
+                useCaseManager.getSingularEvent(eventID, userID);
         if (meta == null) {
             return null;
         }
-        return EventAdapter.createEventSingularModel(meta.getFirst(), meta.getSecond(), meta.getThird(), meta.getFourth(), meta.getFifth(), meta.getSixth());
+        return EventAdapter.createEventSingularModel(meta.getFirst(), meta.getSecond(), meta.getThird(),
+                meta.getFourth(), meta.getFifth(), meta.getSixth());
     }
+
+    /**
+     * Get event directions URL
+     *
+     * @param eventID ID of event
+     * @return URL of event directions
+     */
     public String getEventDirections(String eventID) {
         return useCaseManager.getEventDirections(eventID);
     }
+
+    /**
+     * Get event weather URL
+     *
+     * @param eventID ID of event
+     * @return URL for event weather
+     */
     public String getEventWeather(String eventID) {
         return useCaseManager.getEventWeather(eventID);
     }
+
+    /**
+     * Get event Twitter share URL
+     *
+     * @param eventID ID of event
+     * @return URL to share event on Twitter
+     */
     public String getEventTwitterShare(String eventID) {
         return useCaseManager.getEventTwitterShare(eventID);
     }
+
+    /**
+     * Get event email share URL
+     *
+     * @param eventID ID of event
+     * @return event email share URL
+     */
     public String getEventEmailShare(String eventID) {
         return useCaseManager.getEventEmailShare(eventID);
     }
+
+    /**
+     * Edit an event name
+     *
+     * @param eventID ID of event
+     * @param name new name
+     * @param ownerID ID of event owner
+     * @return whether edit was successful
+     */
     public boolean editEventName(String eventID, String name, String ownerID) {
         return useCaseManager.editEventName(eventID, name, ownerID);
     }
+
+    /**
+     * Edit an event location
+     *
+     * @param eventID ID of event
+     * @param location new location
+     * @param ownerID ID of owner of event
+     * @return whether edit was successful
+     */
     public boolean editEventLocation(String eventID, String location, String ownerID) {
         return useCaseManager.editEventLocation(eventID, location, ownerID);
     }
+
+    /**
+     * Edit an event's times
+     *
+     * @param eventID ID of event
+     * @param start new start time string
+     * @param end new end time string
+     * @param ownerID ID of owner of event
+     * @return whether edit was successful
+     */
     public boolean editEventTime(String eventID, String start, String end, String ownerID) {
         LocalDateTime startTime = GlobalAdapter.stringToDateTime(start);
         LocalDateTime endTime = GlobalAdapter.stringToDateTime(end);
         return useCaseManager.editEventTime(eventID, startTime, endTime, ownerID);
     }
+
+    /**
+     * Delete an event
+     *
+     * @param eventID ID of event to delete
+     * @param ownerID ID of owner of event
+     * @return whether deletion was successful
+     */
     public boolean deleteEvent(String eventID, String ownerID) {
         return useCaseManager.deleteEvent(eventID, ownerID);
     }
 
+    /**
+     * Get list model of events with a name
+     *
+     * @param name name to filter events by
+     * @param userID ID of user associated with the events
+     * @return ListModel of events
+     */
     public ListModel getEventsByName(String name, String userID) {
         return EventAdapter.createEventListModel(useCaseManager.getEventsByNameAndUserID(name, userID));
     }
+
+    /**
+     * Get list model of events in the past
+     *
+     * @param userID ID of user associated with events
+     * @return list model of events
+     */
     public ListModel getPastEvents(String userID) {
         return EventAdapter.createEventListModel(useCaseManager.getPastEvents(userID));
     }
+
+    /**
+     * Get list model of events in the present
+     *
+     * @param userID ID of user associated with events
+     * @return list model of events
+     */
     public ListModel getCurrentEvents(String userID) {
         return EventAdapter.createEventListModel(useCaseManager.getCurrentEvents(userID));
     }
+
+    /**
+     * Get list model of events in the future
+     *
+     * @param userID ID of user associated with events
+     * @return list model of events
+     */
     public ListModel getFutureEvents(String userID) {
         return EventAdapter.createEventListModel(useCaseManager.getFutureEvents(userID));
     }
 
+    /**
+     * Get list model of time strings and events by an hour time frame
+     *
+     * @param date date of the time frame
+     * @param hour hour of the time frame
+     * @param userID ID of user associated with the events
+     * @return list model of times and events
+     */
     public ListModel getHourlyEvents(String date, int hour, String userID) {
         LocalDate dateObject = GlobalAdapter.stringToDate(date);
         Tuple<ArrayList<LocalDateTime>, ArrayList<ArrayList<CalendarEvent>>> eventTimes = useCaseManager.getEventsByHour(dateObject, hour, userID);
         return EventAdapter.createEventDateListModel(eventTimes.getSecond(), eventTimes.getFirst());
     }
+
+    /**
+     * Get list model of time strings and events by a day time frame
+     *
+     * @param date day of time frame
+     * @param userID ID of user associated with the events
+     * @return list model of times and events
+     */
     public ListModel getDailyEvents(String date, String userID) {
         LocalDate dateObject = GlobalAdapter.stringToDate(date);
         Tuple<ArrayList<LocalDateTime>, ArrayList<ArrayList<CalendarEvent>>> eventTimes = useCaseManager.getEventsByDay(dateObject, userID);
         return EventAdapter.createEventDateListModel(eventTimes.getSecond(), eventTimes.getFirst());
     }
+
+    /**
+     * Get list model of time strings and events by a weekly time frame
+     *
+     * @param date first day of the week time frame
+     * @param userID ID of user associated with the events
+     * @return list model of times and events
+     */
     public ListModel getWeeklyEvents(String date, String userID) {
         LocalDate dateObject = GlobalAdapter.stringToDate(date);
         Tuple<ArrayList<LocalDateTime>, ArrayList<ArrayList<CalendarEvent>>> eventTimes = useCaseManager.getEventsByWeek(dateObject, userID);
         return EventAdapter.createEventDateListModel(eventTimes.getSecond(), eventTimes.getFirst());
     }
+
+    /**
+     * Get list model of time strings and events by a monthly time frame
+     *
+     * @param year year of the time frame
+     * @param month month of the time frame
+     * @param userID ID of user associated with the events
+     * @return list model of times and events
+     */
     public ListModel getMonthlyEvents(int year, int month, String userID) {
         Month monthObject = GlobalAdapter.intToMonth(month);
         Tuple<ArrayList<LocalDateTime>, ArrayList<ArrayList<CalendarEvent>>> eventTimes = useCaseManager.getEventsByMonth(year, monthObject, userID);
         return EventAdapter.createEventDateListModel(eventTimes.getSecond(), eventTimes.getFirst());
     }
 
+    /**
+     * Get list model of events associated with an alert
+     *
+     * @param alertID ID of alert events are associated with
+     * @param ownerID ID of the owner of the events
+     * @return list model of events
+     */
     public ListModel getEventsFromAlert(String alertID, String ownerID) {
         return EventAdapter.createEventListModel(useCaseManager.getEventsByAlertID(alertID, ownerID));
     }
 
+    /**
+     * Get list model of events associated with a series
+     *
+     * @param seriesID ID of series associated with event
+     * @param ownerID ID of owner of the events
+     * @return list model of events
+     */
     public ListModel getEventsBySeriesIDAndOwnerID(String seriesID, String ownerID) {
         return EventAdapter.createEventListModel(useCaseManager.getEventsBySeriesIDAndOwnerID(seriesID, ownerID));
     }
 
+    /**
+     * Get list model of events associated with a tag
+     *
+     * @param tagID ID of associated tag
+     * @param ownerID ID of owner of the events
+     * @return ListModel of events
+     */
     public ListModel getEventsByTagIDAndOwnerID(String tagID, String ownerID){
         return EventAdapter.createEventListModel(useCaseManager.getEventsByTagIDAndOwnerID(tagID, ownerID));
     }
 
     // INVITATIONS
+
+    /**
+     * Get list model of incoming invitations
+     *
+     * @param userID ID of user receiving invitations
+     * @return list model of invitations
+     */
     public ListModel getIncomingInvitations(String userID) {
         return InvitationAdapter.createInvitationListModel(useCaseManager.getIncomingInvitations(userID));
     }
+
+    /**
+     * Get list model of outgoing invitations
+     *
+     * @param userID ID of user sending these invitations
+     * @return list model of invitations
+     */
     public ListModel getOutgoingInvitations(String userID) {
         return InvitationAdapter.createInvitationListModel(useCaseManager.getOutgoingInvitations(userID));
     }
+
+    /**
+     * Get a singular model for invitation
+     *
+     * @param invitationID ID of invitation
+     * @param userID ID of user associated with invitation
+     * @return singular view model for invitation
+     */
     public SingularInvitationModel getSingularInvitation(String invitationID, String userID) {
         return InvitationAdapter.createInvitationSingularModel(useCaseManager.getInvitation(invitationID, userID));
     }
+
+    /**
+     * Accept or decline an invitation
+     *
+     * @param invitationID ID of invitation
+     * @param respondingMessage message response to invitation
+     * @param accept whether to accept or decline the invitation
+     * @param userID ID of the recipient of the invitation
+     * @return
+     */
     public boolean acceptInvitation(String invitationID, String respondingMessage, Boolean accept, String userID) {
         return useCaseManager.acceptInvitation(invitationID, respondingMessage, accept, userID);
     }
+
+    /**
+     * Create an invitation.
+     *
+     * @param eventID ID of event of the invitation
+     * @param inviterID ID of the inviter
+     * @param inviteeID ID of invitee
+     * @param initialMessage initial message to invitee
+     * @return invitation ID if successful, or null otherwise
+     */
     public String createInvitation(String eventID, String inviterID, String inviteeID, String initialMessage) {
         return useCaseManager.createInvitation(eventID, inviterID, inviteeID, initialMessage);
     }
+
+    /**
+     * Delete an invitation
+     *
+     * @param invitationID ID of invitation to delete
+     * @param userID ID of inviter
+     * @return whether invitation deletion was successful
+     */
     public boolean deleteInvitation(String invitationID, String userID) {
         return useCaseManager.deleteInvitation(invitationID, userID);
     }
@@ -262,23 +499,61 @@ public class Controller {
     }
 
     // TIME
+
+    /**
+     * Get a string representation of the current application time
+     *
+     * @return current time string
+     */
     public String getTime() {
         return GlobalAdapter.dateToString(useCaseManager.getTime());
     }
+
+    /**
+     * Set application time according to date string input.
+     *
+     * @param dateTime date string to set time to
+     */
     public void setTime(String dateTime) {
         useCaseManager.setTime(GlobalAdapter.stringToDateTime(dateTime));
     }
+
+    /**
+     * Reset the application time
+     */
     public void resetTime() {
         useCaseManager.resetTime();
     }
 
     // USER
+
+    /**
+     * Create a new user
+     *
+     * @param username user's username
+     * @param password user's password
+     * @return whether user was created successfully
+     */
     public boolean createUser(String username, String password) {
         return useCaseManager.createUser(username, password);
     }
+
+    /**
+     * Login a user using username and password
+     *
+     * @param username user's username
+     * @param password user's password
+     * @return user ID if successful, or
+     */
     public String loginUser(String username, String password) {
         return useCaseManager.loginUser(username, password);
     }
+
+    /**
+     * Get a ListModel for all application users
+     *
+     * @return ListModel of user
+     */
     public ListModel getUsers() {
         return UserAdapter.createUserListModel(useCaseManager.getUsers());
     }
